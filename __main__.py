7b924c6b22565a9dffa739980e6b1b52819a3733
Battle-Of-Two-K <tr.penrose@mail.ru>
2021-03-25 02:16:54 +0300

    Оформил красивый вывод данных в консоль

--- __main__.py
+++ __main__.py
@@ -2,15 +2,6 @@ from tkinter import Tk, Canvas
 import json
 
 
-def read_data_json_file():
-    """
-    Метод считывает данные из файла 'Input_data.json'
-    """
-    with open('Input_data.json', encoding="utf-8") as file:
-        data = json.loads(file.read())
-        print(data["Входные данные"]["Отклонение"])
-
-
 class App:
     canvas_opts = {
         'width': 720,
@@ -18,27 +9,39 @@ class App:
         'bg': 'black'
     }
 
-    input_data = {}
-    parameters = ['Отклонение', 'Размер куба',
-                  'Диаметр пружины', 'Шаг витков пружины']
+    data = {}
 
     def __init__(self):
         self.root = Tk()
         self.canvas = Canvas(self.root, **self.canvas_opts)
         self.canvas.pack()
 
-        read_data_json_file()
-        # self.decor()
+        self.read_data_json_file()
+        self.decor()
+
+        # print(self.input_data.items())
         # input()
 
+    def read_data_json_file(self):
+        """
+        Метод считывает данные из файла 'Input_data.json'
+        """
+        with open('Input_data.json', encoding="utf-8") as file:
+            self.data = json.loads(file.read())
 
     def decor(self):
-        print('Входные данные:')
-        for i, j in zip(self.input_data, self.input_data.values()):
-            print(f' - {i}: {j}')
+        for key, value in self.data.items():
+            print()
+            print(f"{key}:")
 
-    def write_text(self, data: str):
-        self.canvas.create_text(100, 100, text=data, font=('Comic Sans MS', 30))
+            if isinstance(value, dict):
+                for inside_key, inside_value in value.items():
+                    print(f"    {inside_key}: {inside_value}")
+            elif isinstance(value, list):
+                for step in value:
+                    print(f"    {step}")
+            else:
+                print(f"    {key}: {value}\n")
 
     def run(self):
         self.root.mainloop()
