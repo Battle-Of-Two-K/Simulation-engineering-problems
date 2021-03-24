ee4eb13c806017c6bdb2ddd74ef0738d9bd91a8a
Battle-Of-Two-K <tr.penrose@mail.ru>
2021-03-25 00:56:13 +0300

    Второй вариант

--- __main__.py
+++ __main__.py
@@ -1,4 +1,14 @@
 from tkinter import Tk, Canvas
+import json
+
+
+def read_data_json_file():
+    """
+    Метод считывает данные из файла 'Input_data.json'
+    """
+    with open('Input_data.json', encoding="utf-8") as file:
+        data = json.loads(file.read())
+        print(data["Входные данные"]["Отклонение"])
 
 
 class App:
@@ -8,43 +18,24 @@ class App:
         'bg': 'black'
     }
 
-    input_data = {'Отклонение': -1,
-                  'Размер куба': -1,
-                  'Диаметр пружины': -1,
-                  'Шаг витков пружины':-1}
+    input_data = {}
+    parameters = ['Отклонение', 'Размер куба',
+                  'Диаметр пружины', 'Шаг витков пружины']
 
     def __init__(self):
         self.root = Tk()
         self.canvas = Canvas(self.root, **self.canvas_opts)
         self.canvas.pack()
 
-        self.read_data_from_file()
-        print(self.input_data)
+        read_data_json_file()
+        # self.decor()
+        # input()
 
-    def read_data_from_file(self):
-        """
-        Метод считывает данные из файла 'Input_data.txt'
-        """
-        with open('Input_data.txt', encoding="utf-8") as file:
-            strings = file.readlines()
 
-        # for i in strings:
-        #     if i.index('Отклонение') == 0:
-        #         self.input_data['Отклонение'] = int(i.split()[-1])
-        #
-        #     elif i.index('Размер куба') == 0:
-        #         self.input_data['Размер куба'] = int(i.split()[-1])
-        #
-        #     elif i.index('Диаметр пружины') == 0:
-        #         self.input_data['Диаметр пружины'] = int(i.split()[-1])
-        #
-        #     elif i.index('Шаг витков пружины') == 0:
-        #         self.input_data['Шаг витков пружины'] = int(i.split()[-1])
-
-        self.input_data['Отклонение'] = int(strings[0].split()[-1])
-        self.input_data['Размер куба'] = int(strings[1].split()[-1])
-        self.input_data['Диаметр пружины'] = int(strings[2].split()[-1])
-        self.input_data['Шаг витков пружины'] = int(strings[3].split()[-1])
+    def decor(self):
+        print('Входные данные:')
+        for i, j in zip(self.input_data, self.input_data.values()):
+            print(f' - {i}: {j}')
 
     def write_text(self, data: str):
         self.canvas.create_text(100, 100, text=data, font=('Comic Sans MS', 30))
